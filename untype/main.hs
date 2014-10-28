8fd95623af116bd8c6424f4db526498c4925d2b8
ajiyoshi-vg <Yoichi_Sudo@voyagegroup.com>
2014-10-28 23:48:24 +0900

    untyped

--- untype/main.hs
+++ untype/main.hs
@@ -1,6 +1,14 @@
 import Untyped
 
-main :: IO ()
-main =
-  putStr "wip"
+repl :: Monad m => (Term -> m b) -> [String] -> m ()
+repl puts = mapM_ ( puts . eval . read )
 
+test :: IO ()
+test = repl ( print . show ) [
+            "TmVar 0 0"
+            , "Lambda (TmVar 0 0)"
+            , "TmApp (Lambda (TmVar 0 0)) (Lambda (TmApp (TmVar 0 0) (TmVar 0 0)))"
+            ]
+
+main :: IO ()
+main = test
--- untype/untyped.hs
+++ untype/untyped.hs
@@ -1,5 +1,6 @@
 module Untyped (
-termShift
+Term
+, termShift
 , termSubst
 , termSubstTop
 , isval
@@ -7,9 +8,7 @@ termShift
 , eval
 ) where
 
-type Val = Integer
-type Len = Integer
-data Term = TmVar Val Len | TmAbs Term | TmApp Term Term deriving ( Show )
+data Term = TmVar Integer Integer | Lambda Term | TmApp Term Term deriving ( Show, Read )
 
 --type Context = [(String, String)] 
 
@@ -17,10 +16,10 @@ termShift :: Integer -> Term -> Term
 termShift d term =
   let
     walk c t = case t of
-      TmVar val len
-        | len >= c -> TmVar (val+d) (len+d)
-        | otherwise -> TmVar val (len+d)
-      TmAbs t1 -> TmAbs $ walk (c+1) t1
+      TmVar x n
+        | x >= c -> TmVar (x+d) (n+d)
+        | otherwise -> TmVar x (n+d)
+      Lambda t1 -> Lambda $ walk (c+1) t1
       TmApp t1 t2 -> TmApp (walk c t1) (walk c t2)
   in walk 0 term
     
@@ -28,10 +27,10 @@ termSubst :: Integer -> Term -> Term -> Term
 termSubst j s term =
   let
     walk c t = case t of
-      TmVar val len
-        | len == j + c -> termShift c s
-        | otherwise -> TmVar val len
-      TmAbs t1 -> TmAbs (walk (c+1) t1)
+      TmVar x n
+        | x == j + c -> termShift c s
+        | otherwise -> TmVar x n
+      Lambda t1 -> Lambda (walk (c+1) t1)
       TmApp t1 t2 -> TmApp (walk c t1) (walk c t2)
   in walk 0 term
 
@@ -41,21 +40,23 @@ termSubstTop s t =
 
 isval :: Term -> Bool
 isval term = case term of
-    TmAbs _ -> True
+    Lambda _ -> True
     _ -> False 
 
 eval1 :: Term -> Maybe Term
 eval1 term = case term of
-  TmApp (TmAbs t12) v2 | isval(v2) -> Just ( termSubstTop v2 t12 )
-  TmApp v1 t2 | isval(v1) -> do
+  TmApp (Lambda t12) v2
+    | isval v2 -> Just ( termSubstTop v2 t12 )
+  TmApp v1 t2 | isval v1 -> do
     t2' <- eval1 t2
     return ( TmApp v1 t2' )
   TmApp t1 t2 -> do
     t1' <- eval1 t1
     return ( TmApp t1' t2 )
+  _ -> Nothing
 
 eval :: Term -> Term
 eval term =
-  case (eval1 term) of
+  case eval1 term of
     Just t -> eval t
     Nothing -> term 
